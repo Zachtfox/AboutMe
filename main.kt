a9848aad28ce82dd77498fc2412ff14461202b02
Zachtfox <31944870+Zachtfox@users.noreply.github.com>
2020-10-08 15:35:48 -0500

    Add files via upload

---
+++ main.kt
@@ -0,0 +1,6 @@
+fun main(){
+    println("Hello! My name is Zach Fox.")
+    println("Currently, I work as a supervisor at a grocery store.")
+    println("Coronavirus has made retail life even worse!")
+    println("We didn't think that was possible!")
+}
